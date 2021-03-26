4e5d5a3929a78adb19150d724c3017a9f19c3708
CAMANEM <camanem10@hotmail.com>
2021-03-26 14:50:47 -0600

    jar

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hola mundus");
+        Calculator calculator = new Calculator();
+        calculator.init();
     }
 }
